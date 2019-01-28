b62ceabeb0f68c7a2b9a63220b55d249765e33b6
Arunda Victor <victoroyugi31@gmail.com>
2019-01-28 19:11:42 +0300

    add new tests for encoding

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,30 @@
- // public static void main(String[] args) 
- //    { 
- //        String text = "ATTACKATONCE"; 
- //        int s = 4; 
- //        System.out.println("Text  : " + text); 
- //        System.out.println("Shift : " + s); 
- //        System.out.println("Cipher: " + encrypt(text, s)); 
- //    } 
+// import java.io.Console;
+
+// public class App {
+//   public static void main(String[] args) {
+//   	  Console myConsole = System.console();
+//     System.out.println("Enter the sentence to encrypt:");
+//     String encryptFile = myConsole.readLine();
+
+//     System.out.println("Enter the key:");
+//     String encryptkey = myConsole.readLine();
+//   }
+// }
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Encoding encoding = new Encoding("","",22,"");
+
+// encoding
+
+        System.out.println("Add your string");
+        String userString = scanner.nextLine();
+        encoding.setMinput(userString);
+        encoding.encrypt(userString,22);
+        System.out.println("Input: " + encoding.getMinput() );
+        System.out.println("Encoded: " + encoding.getResult());
+	      }
+	  }
--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,24 +1,99 @@
-// class Cipher 
-// { 
-//     // Encrypts text using a shift od s 
-//     public static StringBuffer encrypt(String text, int s) 
-//     { 
-//         StringBuffer result= new StringBuffer(); 
-  
-//         for (int i=0; i<text.length(); i++) 
-//         { 
-//             if (Character.isUpperCase(text.charAt(i))) 
-//             { 
-//                 char ch = (char)(((int)text.charAt(i) + 
-//                                   s - 65) % 26 + 65); 
-//                 result.append(ch); 
-//             } 
-//             else
-//             { 
-//                 char ch = (char)(((int)text.charAt(i) + 
-//                                   s - 97) % 26 + 97); 
-//                 result.append(ch); 
-//             } 
-//         } 
-//         return result; 
-//     } 
+public class Encoding {
+
+    private String minput;
+    private int mkey;
+    Boolean res = false;
+    private String moutput;
+    private String result;
+
+    public Encoding(String input, String output, int key, String mresult) {
+        minput = input;
+        moutput = output;
+        mkey = key;
+        result = mresult;
+    }
+
+    public boolean textInput(String input){
+
+        for (int i = 0; i < input.length(); i++)
+            if (Character.isDigit(input.charAt(i)) == false){
+                res = true;
+            }else {
+                res = false;
+            }
+        return res;
+    }
+
+
+    public boolean textOutput(String output){
+        return true;
+    }
+
+    public String getMinput() {
+        return minput;
+    }
+
+    public void setMinput(String minput) {
+        this.minput = minput;
+    }
+
+    public int getMkey() {
+        return mkey;
+    }
+
+    public void setMkey(int mkey) {
+        this.mkey = mkey;
+    }
+
+    public String getMoutput() {
+        return moutput;
+    }
+
+    public void setMoutput(String moutput) {
+        this.moutput = moutput;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+
+
+    public String encrypt(String input, int key){
+        int cipherLength = input.length();
+        for (int i = 0; i < cipherLength; i++){
+            char ch = input.charAt(i);
+
+            if (Character.isLetter(ch)){
+                if (Character.isLowerCase(ch)){
+                    char c = (char)(ch+key);
+                    if (c>'z'){
+                        result += (char)(ch - (25-key));
+                    }else{
+                       result += c;
+                    }
+                }else if (Character.isUpperCase(ch)){
+                    char c = (char)(ch+key);
+                    if (c>'Z'){
+                        result += (char)(ch - (25-key));
+                    }else{
+                        result += c;
+                    }
+                }
+
+            }else{
+
+                result += ch;
+            }
+
+        }
+
+        return "true";
+
+    }
+
+}
