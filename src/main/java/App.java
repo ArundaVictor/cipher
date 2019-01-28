1d788df5f7207c07ee0876b75ea39b07d984775c
Arunda Victor <victoroyugi31@gmail.com>
2019-01-28 19:46:38 +0300

    add licemce information

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,29 +2,29 @@
 
 // public class App {
 //   public static void main(String[] args) {
-//   	  Console myConsole = System.console();
+  	  // Console myConsole = System.console();
 //     System.out.println("Enter the sentence to encrypt:");
-//     String encryptFile = myConsole.readLine();
+    // String encryptFile = myConsole.readLine();
 
 //     System.out.println("Enter the key:");
 //     String encryptkey = myConsole.readLine();
 //   }
 // }
+import java.io.Console;
 
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Encoding encoding = new Encoding("","",22,"");
+    	Console myConsole = System.console();
+        Encoding encoding = new Encoding("","",5,"");
 
-// encoding
 
-        System.out.println("Add your string");
-        String userString = scanner.nextLine();
+
+        System.out.println("Enter the text you want to encrypt:");
+         String userString = myConsole.readLine();
         encoding.setMinput(userString);
-        encoding.encrypt(userString,22);
+        encoding.encrypt(userString,5);
         System.out.println("Input: " + encoding.getMinput() );
-        System.out.println("Encoded: " + encoding.getResult());
+        System.out.println("Encoded text is: " + encoding.getResult());
 	      }
 	  }
